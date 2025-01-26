36c97220fe477365a5ec388e75d0ea454025f853
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-01-26 15:12:29 -0500

    feat(routers): add auth, contact, phone, and user router

---
+++ api/app/routers/auth_router.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from routers.dependencies import oauth2_schema
+from services.auth_service import AuthService
+
+auth_router = APIRouter(
+    prefix='/auth',
+    tags=['Authentication']
+)
+
+auth_service = AuthService()
+
+
+@auth_router.post('/signin')
+def signin(user: OAuth2PasswordRequestForm = Depends()):
+    access_token = auth_service.authenticate(user)
+    return access_token
+
+
+@auth_router.get('/profile')
+def profile(token: str = Depends(oauth2_schema)):
+    return auth_service.decode_token(token)
